be72066544b17b2f8d67f711878bf5168a12e3f3
zhangyanling77 <18783226594@163.com>
2019-08-06 14:03:48 +0800

    Create Foo.vue

---
+++ Foo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hi, Foo
+    <button @click="clickMe">click me</button>
+    <h1>{{this.$store.state.name}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch('changeName')
+  },
+  asyncData(store) { // 该方法只在服务端执行，并且只在页面组件中执行
+  console.log('server run')
+    return store.dispatch('changeName')
+  },
+  methods: {
+    clickMe() {
+      alert("hello~~")
+    }
+  }
+}
+</script>
