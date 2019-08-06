d7c7b3e813147483bd9119d5236f091330ce7faa
zhangyanling77 <18783226594@163.com>
2019-08-06 14:02:11 +0800

    Create main.js
    程序入口文件

---
+++ main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App'
+import createRouter from './router'
+import createStore from './store'
+
+
+export default () => {
+  const router = createRouter()
+  const store = createStore()
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  })
+
+  return { app, router, store }
+}
