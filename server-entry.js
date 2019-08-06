51d4b10d6b8e68ada674f8a317f8d26056639456
zhangyanling77 <18783226594@163.com>
2019-08-06 13:58:09 +0800

    Create server-entry.js
    服务端入口文件

---
+++ server-entry.js
@@ -0,0 +1,38 @@
+// 服务端入口
+import createApp from './main'
+
+// 服务端需要调用当前文件产生一个app实例
+
+export default (context) => {
+  // 服务端将执行此方法
+  return new Promise((resolve, reject) => {
+    const { app, router, store } = createApp()
+    // 返回实例 渲染当前页面
+    router.push(context.url)
+    // 涉及到异步组件
+    router.onReady(() => {
+      // 获取当前跳转到的匹配的组件 matchs匹配到的所有组件， 整个都在服务端执行
+      let matchs = router.getMatchedComponents()
+
+      if(matchs.length === 0) {
+        reject({code: 404})
+      }
+      //
+      Promise.all(matchs.map(component => {
+        if(component.asyncData){
+          return component.asyncData(store)
+        }
+      })).then(() => {
+        // 以上all中的方法会改变store中的state
+        context.state = store.state // 把vuex状态挂载到window上
+        resolve(app)
+      })
+      
+    }, reject)
+  })
+
+  // 获取数据的操作 在服务端获取好完整的数据
+  
+}
+
+// 需要导出给node使用
